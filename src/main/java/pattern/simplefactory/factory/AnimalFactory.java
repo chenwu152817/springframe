2a3dc1de2e639d19cec69bc1787cc495351ddb50
potato <906438950@qq.com>
2020-05-21 22:26:02 +0800

    简单工厂模式注解

--- src/main/java/pattern/simplefactory/factory/AnimalFactory.java
+++ src/main/java/pattern/simplefactory/factory/AnimalFactory.java
@@ -8,6 +8,12 @@ import pattern.simplefactory.entity.Dog;
  * @author jjj
  * @date 2020/5/21 22:06
  */
+
+/**
+ * 简单工厂模式
+ * 多类继承或实现同一接口
+ * 给工厂类传入类型参数创建实例
+ */
 public class AnimalFactory {
     public static Animal createAnimal(int type){
         switch (type){
