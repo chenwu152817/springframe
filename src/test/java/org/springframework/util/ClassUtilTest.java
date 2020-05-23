260369cfee1be4fba20554c6ae40d5410b116c6d
potato <906438950@qq.com>
2020-05-24 00:31:25 +0800

    根据包名加载类文件方法及测试

---
+++ src/test/java/org/springframework/util/ClassUtilTest.java
@@ -0,0 +1,19 @@
+package org.springframework.util;
+
+
+import org.junit.Test;
+
+import java.util.Set;
+
+/**
+ * @author jjj
+ * @date 2020/5/23 23:44
+ */
+public class ClassUtilTest {
+
+    @Test
+    public void extractPackageClassTest(){
+        Set<Class<?>> classSet = ClassUtil.extractPackageClass("demo.pattern.factory.entity");
+        System.out.println(classSet);
+    }
+}
